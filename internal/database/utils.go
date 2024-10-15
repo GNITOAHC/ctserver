c5c6060dba5370adaa135e8cf3f8f3e9212d66f7
GINTOAHC <chaotingchen10@gmail.com>
2024-10-16 02:09:43 +0800

    feat: Update hash utils

--- internal/database/utils.go
+++ internal/database/utils.go
@@ -1,18 +1,56 @@
 package database
 
 import (
-	"crypto/sha1"
+	"crypto/md5"
 	"database/sql"
-	"encoding/base64"
+	"errors"
+	"math/big"
+	"strconv"
 	"time"
 )
 
+const base62Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
 // generateShortened generates a hashed value for the given url
 func generateShortened(url string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(url))
-	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-	return sha[:8] // Return the first 8 characters of the hashed value
+	// Generate MD5
+	hash := md5.Sum([]byte(url)) // Generate MD5
+	hashPrefix := hash[:6]
+
+	num := new(big.Int).SetBytes(hashPrefix) // Convert hash bytes to big int
+
+	// Encode the big int to base62
+	shortened := func(num *big.Int) string {
+		result := ""
+		base := big.NewInt(62)
+		zero := big.NewInt(0)
+
+		for num.Cmp(zero) > 0 {
+			mod := new(big.Int)
+			num.DivMod(num, base, mod)
+			result = string(base62Chars[mod.Int64()]) + result
+		}
+
+		return result
+	}(num)
+
+	return shortened
+}
+
+func rehash(url string, existing map[string]bool) (string, error) {
+	shortened := generateShortened(url)
+	counter := 0
+	for {
+		counter++
+		newUrl := generateShortened(url + strconv.Itoa(counter))
+		if _, exists := existing[newUrl]; !exists {
+			return shortened, nil
+		}
+		if counter > 100 {
+			break
+		}
+	}
+	return "", errors.New("hash collision")
 }
 
 // NullString returns a sql.NullString from the given string
